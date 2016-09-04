f311d12732d8e36ca4d875e8d7ec710d30fe5f6a
Christopher Berry <cnberry@gmail.com>
2016-09-03 21:14:19 -0400

    update counter with Makefile and dump.vcd

--- counter/counter_tb.v
+++ counter/counter_tb.v
@@ -1,23 +1,78 @@
-module test;
+// ----------------------------------------------------------------------
+// Author: Chris Berry (cnberry@gmail.com)
+// Date: September 3, 2016
+// Description: Verilog version of a 'Hello World' program. Simple tb and counter
+// ------------------------------------------------------------------------
 
-  /* Make a reset that pulses once. */
-  reg reset = 0;
-  initial begin
-     # 17 reset = 1;
-     # 11 reset = 0;
-     # 29 reset = 1;
-     # 11 reset = 0;
-     # 100 $stop;
+`timescale 1 ns / 100 ps
+
+module counter_tb ;
+
+reg Clk_tb;
+reg Reset_tb;
+reg Clk_cnt;
+
+//Instantiate module under test (DUT)
+counter DUT(.out(),.clk(Clk_tb),.reset(Reset_tb));
+
+//Specify dump file for use with GTKWave
+initial
+  begin  : DUMPFILE
+    $dumpfile("dump.vcd");
+    $dumpvars(0,counter_tb);
   end
 
-  /* Make a regular pulsing clock. */
-  reg clk = 0;
-  always #5 clk = !clk;
+//Create a 100MHz clock
+parameter CLK_PERIOD = 10;
+initial
+  begin  : CLK_GENERATOR
+    $display("begin CLK_GENERATOR");
+    Clk_tb = 0;
+    forever
+       begin
+	      #(CLK_PERIOD/2) Clk_tb = ~Clk_tb;
+       end 
+  end
 
-  wire [7:0] value;
-  counter c1 (value, clk, reset);
+//Generate a 2 clk wide reset
+initial
+  begin  : RESET_GENERATOR
+    $display("begin RESET_GENERATOR");
+    Reset_tb = 1;
+    #(2 * CLK_PERIOD) Reset_tb = 0;
+  end
 
-  initial
-     $monitor("At time %t, value = %h (%0d)",
-              $time, value, value);
-endmodule // test
+//Count clocks. Sometime this is usefull
+initial
+  begin  : CLK_COUNTER
+    Clk_cnt = 0;
+    $display("begin CLK_COUNTER");
+    forever
+       begin
+	      #(CLK_PERIOD) Clk_cnt = Clk_cnt + 1;
+       end 
+  end
+
+//Main part of testbench, do stuff or just wait
+initial
+  begin  : STIMULUS
+    $display("\nbegin STIMULUS");
+
+    wait (!Reset_tb);    // wait until reset is over
+    @(posedge Clk_tb);    // wait for a clock edge
+    $display("\tRESET over");
+
+    ////////////////////////////////////////
+    // Put your stimulus here
+
+    #1000	//Wait 1000ns just to do something
+
+    ///////////////////////////////////////
+
+    $display("end STIMULUS");
+    $finish;  
+	          
+  end // STIMULUS
+
+
+endmodule  // counter_tb
