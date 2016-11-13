1bcb2d5f0d4f2a71b5996d7814f9556cac6667de
Christopher Berry <cnberry@gmail.com>
2016-11-13 17:38:10 -0500

    assert for cout, save dump for viewing later

--- adder/adder_tb.v
+++ adder/adder_tb.v
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 // Author: Chris Berry (cnberry@gmail.com)
 // Date: September 3, 2016
-// Description: Verilog version of a 'Hello World' program. Simple tb and counter
+// Description: Verilog version of adder. Simple tb, task, for loop 
 // ------------------------------------------------------------------------
 
 `timescale 1 ns / 100 ps
@@ -10,10 +10,23 @@ module adder_tb ;
 
 reg Clk_tb;
 reg Reset_tb;
-reg Clk_cnt;
+reg [31:0] Clk_cnt;
+reg a;
+reg b;
+reg c_in;
+
+integer i,j,k;
+wire s1,s2,s3;
+wire c1,c2,c3;
 
 //Instantiate module under test (DUT)
-adder DUT(.sum(),.c_in(),c_out(),.a(),.b());
+adder_dataflow ADD_dataflow(.sum(s1),.c_out(c1),.c_in(c_in),.a(a),.b(b));
+adder_behavioral ADD_behavioral(.sum(s2),.c_out(c2),.c_in(c_in),.a(a),.b(b));
+adder_structural ADD_structural(.sum(s3),.c_out(c3),.c_in(c_in),.a(a),.b(b));
+
+//Instantiate synchronous assertion checks (self checking TB)
+assert a_sum(.clk(Clk_tb),.expression(s1 === s2 && s2 === s3));
+assert a_cout(.clk(Clk_tb),.expression(c1 === c2 && c2 === c3));
 
 //Specify dump file for use with GTKWave
 initial
@@ -65,14 +78,48 @@ initial
     ////////////////////////////////////////
     // Put your stimulus here
 
-    #1000	//Wait 1000ns just to do something
+    // Loop through all input combinations
+    for (i=0;i<2;i++)
+      for (j=0;j<2;j++)
+	for (k=0;k<2;k++)
+    	  test_vector(i,j,k);
+
+    #10		//Wait 10ns just to do something
 
     ///////////////////////////////////////
 
     $display("end STIMULUS");
+    $display("PASSED");
     $finish;  
 	          
   end // STIMULUS
 
+// Task to drive test sequenes
+task test_vector;
 
-endmodule  // counter_tb
+  input a_val, b_val, c_val;
+
+  begin  : TASK_TEST_VECTOR
+        a = a_val;
+        b = b_val;
+        c_in = c_val;
+        @(posedge Clk_tb);    // wait for a clock edge
+        a = 'b0;
+        b = 'b0;
+        c_in = 'b0;
+        @(posedge Clk_tb);    // wait for a clock edge
+  end
+endtask // testvecotr
+
+endmodule  // adder_tb
+
+module assert(input clk,input expression);
+  always @(posedge clk)
+  begin
+    if (expression !== 1'b1)
+    begin
+      $display("ASSERTION FAILED in %m");	 
+      $finish;
+    end
+  end
+endmodule // assert
